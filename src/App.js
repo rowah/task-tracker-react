880db5094ecd4f52481602ffef0f546034e02cd7
Rowa <jrowah@gmail.com>
2022-10-04 12:02:59 +0300

    switches back to function component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-//if we use class component instead of a function component
-import React from "react";
 import Header from "./components/Header";
-// function App() {
-//   return <Header />;
-// }
-class App extends React.Component {
-  render() {
-    return <h1>Hello from a Class Component</h1>;
-  }
+function App() {
+  return <Header />;
 }
 
 export default App;
