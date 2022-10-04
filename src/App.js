ce43cc46ef900ec435dfc525f6b0d20eba69e1c0
Rowa <jrowah@gmail.com>
2022-10-04 11:59:48 +0300

    uses a class component on the app instead of function comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+//if we use class component instead of a function component
+import React from "react";
 import Header from "./components/Header";
-function App() {
-  return <Header />;
+// function App() {
+//   return <Header />;
+// }
+class App extends React.Component {
+  render() {
+    return <h1>Hello from a Class Component</h1>;
+  }
 }
 
 export default App;
