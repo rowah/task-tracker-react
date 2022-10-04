e54152470acae0f9a4eccb4a9fd33916dd04e2d7
Rowa <jrowah@gmail.com>
2022-10-04 13:08:27 +0300

    adds CSS object

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import React from "react";
 const Header = ({ title }) => {
   return (
     <header>
-      <h1 style={{ color: "red", backgroundColor: "orange" }}>{title}</h1>
+      <h1 style={headingStyle}>{title}</h1>
     </header>
   );
 };
@@ -16,6 +16,11 @@ Header.defaultProps = {
 
 Header.propTypes = {
   title: PropTypes.string.isRequired, //if required
+};
+
+const headingStyle = {
+  color: "red",
+  backgroundColor: "black",
 };
 
 export default Header;
