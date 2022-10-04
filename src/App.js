6eb79e3c2317367f74824c53f97f15739777dbdf
Rowa <jrowah@gmail.com>
2022-10-04 11:52:15 +0300

    creates a header component and imports in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
+import Header from "./components/Header";
 function App() {
-  const name = "Rowa";
-  const livesIn = false;
-  const city = "Nairobi";
-  return (
-    //you can only return a single parent element and so putting the h1 out of the div produces an error
-    <div className="container">
-      <h1>
-        Hello {name}, how is {livesIn ? city : "your city?"}
-      </h1>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+//rafcp with the React/React-Native extension installed (creates a boiler plate of a component)
+const Header = () => {
+  return (
+    <header>
+      <h1>Task Tracker</h1>
+    </header>
+  );
+};
+
+export default Header;
