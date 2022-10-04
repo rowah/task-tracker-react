2577e9bff22731170453f7bf7e00adadf42c61d6
Rowa <jrowah@gmail.com>
2022-10-04 14:06:59 +0300

    button click events

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,13 @@ import React from "react";
 
 //deconstructing the default props shows the same title
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Clicked");
+  };
   return (
     <header className="header">
       <h1 style={headingStyle}>{title}</h1>
-      <Button color="green" text="Add" />
+      <Button color="green" text="Add" onClick={onClick} />
       {/* reuses the button component with different props */}
       {/* <Button color="red" text="Hola Two" />
       <Button color="blue" text="Hola Three" /> */}
