c0bfcca00fdaee7c16e4d833d4a2e7841be9c72a
Rowa <jrowah@gmail.com>
2022-10-04 12:27:55 +0300

    deconstructs props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
 import React from "react";
 
-//rafcp with the React/React-Native extension installed (creates a boiler plate of a component)
-const Header = (props) => {
+//deconstructing the default props shows the same title
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
-//create a default prop which would show if none is passed on the App js
-//if the default props also does not exist, nothing shows on h1 since it is not passed on the App .js JSE
-//passing title on the <Header /> in the App js would overwrite the default
 Header.defaultProps = {
   title: "Title from Default Props",
 };
