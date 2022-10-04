20a1ba1f037dc93dea6c785e8881eb8e4d55cfa8
Rowa <jrowah@gmail.com>
2022-10-04 18:27:16 +0300

    makes arr part of state & component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,29 @@
+//use state inside of a function
+import { useState } from "react";
 //rac
-const tasks = [
-  {
-    id: 1,
-    text: "Visit barbershop",
-    day: "Oct 5th at 1600HRS",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Gym members meetup",
-    day: "Oct 8th at 0800HRS",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Morning Run",
-    day: "Oct 9th at 0540HRS",
-    reminder: false,
-  },
-];
 
 const Tasks = () => {
+  //makes the array part of our component
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Visit barbershop",
+      day: "Oct 5th at 1600HRS",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Gym members meetup",
+      day: "Oct 8th at 0800HRS",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Morning Run",
+      day: "Oct 9th at 0540HRS",
+      reminder: false,
+    },
+  ]);
   return (
     <>
       {tasks.map((task) => (
