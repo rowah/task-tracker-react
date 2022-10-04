36813cc7da75a006c73665c01887e2208b1ec859
Rowa <jrowah@gmail.com>
2022-10-04 11:36:13 +0300

    adds conditionals to JSX

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-//is the root app component: everything here gets rendered into the div id="root"
-//where components are added
-// import logo from "./logo.svg";
-// import "./App.css";
-
 function App() {
+  const name = "Rowa";
+  const livesIn = false;
+  const city = "Nairobi";
   return (
-    //you can only return a single parent element
-    <h1>Hello from Nairobi</h1>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    //you can only return a single parent element and so putting the h1 out of the div produces an error
+    <div className="container">
+      <h1>
+        Hello {name}, how is {livesIn ? city : "your city?"}
+      </h1>
+    </div>
   );
 }
 
